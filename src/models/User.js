f99fa51fcc668ef98a7bcd00d1d9ca9631fc6c4d
Federico Giorgino <grgfdrc@gmail.com>
2023-02-05 23:25:21 +0100

    userSchema.pre('save') in User model

--- src/models/User.js
+++ src/models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+const bcrypt = require("bcrypt");
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
@@ -18,6 +18,29 @@ const userSchema = new Schema({
   followers: [{ type: Schema.Types.ObjectId, ref: "User" }],
   following: [{ type: Schema.Types.ObjectId, ref: "User" }],
   visitors: [{ type: Schema.Types.ObjectId, ref: "User" }],
+});
+
+//before saving user to DB
+userSchema.pre("save", function (next) {
+  const user = this;
+  if (!user.isModified("password")) {
+    return next();
+  }
+
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) {
+      return next(err);
+    }
+
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if (err) {
+        return next(err);
+      }
+
+      user.password = hash;
+      next();
+    });
+  });
 });
 
 mongoose.model("User", userSchema);
