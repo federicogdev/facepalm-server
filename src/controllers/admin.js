3e51a62247727d3ef1a72f9da07f2648a5612a0e
Federico Giorgino <grgfdrc@gmail.com>
2023-02-21 20:09:28 +0100

    admin.js getPendingPosts controller

---
+++ src/controllers/admin.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const User = mongoose.model("User");
+const Post = mongoose.model("Post");
+
+exports.getPendingPosts = async (req, res) => {
+  try {
+    const allPendingPosts = await Post.find({
+      approved: false,
+      pending: true,
+    })
+      .populate("user", "username")
+      .sort({ createdAt: -1 });
+
+    res.send({ data: allPendingPosts });
+  } catch (error) {
+    return res.status(500).send({ error: "Server error" });
+  }
+};
